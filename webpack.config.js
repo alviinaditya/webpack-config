75a66d137f6e747a1804818268f5e0c148167ce4
alviinaditya <alviinaditya@gmail.com>
2021-02-13 03:06:13 +0700

    style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,49 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HTMLExtractPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 let mode = "development";
+let target = "web";
 if(process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
-
+    target: target,
+    entry: "./src/index.js",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "dist")
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                excluede: /node_modules/,
+                exclude: /node_modules/,
                 use: {
-                    loader: "babel_loader",
+                    loader: "babel-loader",
                 },
-            }
+            },
+            {
+                test: /\.((c|sa|sc)ss)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
         ],
     },
-    devtool: false,
+
+    plugins: [
+        new MiniCssExtractPlugin({filename:"app.css"}),
+        new HTMLExtractPlugin({template: "./src/index.html"}),
+    ],
+
+    devtool: "source-map",
     devServer: {
         contentBase: "./dist",
     },
